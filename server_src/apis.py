79c53924834ba1eb0a3b413b04d2cc1e7615e37a
Kris_Shin <KrisShin5014@outlook.com>
2021-08-29 00:06:22 +0800

    filter user.

--- server_src/apis.py
+++ server_src/apis.py
@@ -33,8 +33,8 @@ def api_user_list():
         user_list = user_list.filter_by(department_id=department_id)
     if start_time and end_time:
         user_list = user_list.filter(
-            User.create_time > datetime.strptime(start_time, "%Y-%m-%d"),
-            User.create_time < datetime.strptime(end_time, "%Y-%m-%d"),
+            User.create_time >= datetime.strptime(start_time, "%Y-%m-%d"),
+            User.create_time <= datetime.strptime(end_time, "%Y-%m-%d"),
         )
     total = user_list.count()
     user_list = [dict(user) for user in user_list.paginate(page, page_size).items]
